f7c6c9b6add38fed965ccf087130460e111c5d38
Vijurkas <65785008+Sahangis@users.noreply.github.com>
2022-02-20 02:29:46 +0200

    Finished program with C arrays
    pafixinti keli bugai ir pabaigta v0.1 su C masyvais

--- main.cpp
+++ main.cpp
@@ -9,61 +9,64 @@ using namespace std;
 string vardai[16] = { "Martynas", "Jonas", "Vilijamas", "Kristijonas", "Vincas", "Alberas", "Francas" , "Juozas", "Jurgis", "Antanas", "Henrikas", "Balys", "Augustas", "Justinas", "Ernestas", "Hansas" };
 string pavardes[16] = { "Dauksa", "Radvanas", "Marcinkevicius", "Kafka", "Kamiu", "Kudirka", "Shekspyras", "Donelaitis", "Mickevicius", "Biliunas", "Vaizgantas", "Radauskas", "Krivickas", "Savickis", "Sruoga", "Katiliskis" };
 
+const int kiekis = 6;
+
 struct dat {
 	string vardas, pavarde;
-	int pazymiai[10], egzaminas;
+	int pazymiai[kiekis], egzaminas;
 	double answer;
 	int quantity;
 
 };
 
-void generavimas(dat& temp, char answer);
-void input(int i, dat& temp, char answer);
-void isvedimas(dat temp);
-void rasymas(dat temp[], int n, char answer);
+void generavimas(dat& temp, string answer);
+void input(int i, dat& temp, string answer);
+void rasymas(dat temp[], int n, string answer);
+void rikiavimas(int pazymiai[], int n);
 
 int main()
 {
-	char answer;
+	string answer;
 	
 	do {
 		cout << "ar norite ivesti visas reiksmes pats ? (y/n), kitu atveju: programa pati sugeneruos visus duomenis" << endl;
 		cin >> answer;
-		if (answer != 'y' and answer != 'n') cout << "ivesta netinkama reiksme, prasome ivesti vel " << endl;
-	} while (answer != 'y' and answer != 'n');
-	if (answer == 'n')
+		if (answer != "y" and answer != "n") cout << "ivesta netinkama reiksme, prasome ivesti vel " << endl;
+	} while (answer != "y" and answer != "n");
+	if (answer == "n")
 	{
 		srand(time(NULL));
 		int n = (double)rand() / RAND_MAX * 10;
 		do {
 			n = (double)rand() / RAND_MAX * 10;
 		} while (n == 0);
-		char ans = 'v';
+		string ans = "v";
 		int r = (double)rand() / RAND_MAX * 10;
-		if (r>5) ans = 'm';
+		if (r>5) ans = "m";
 		dat* a=new dat[n];
 		for (int i = 0; i < n; i++) generavimas(a[i], ans);
 		rasymas(a, n, ans);
 		delete[] a;
 	}
-	else if (answer == 'y')
+	else if (answer == "y")
 	{
 		int n;
-		
 		do {
-			cout << "iveskite kiek mokiniu bus vedami duomenys (n>0)"<<endl;
+			cout << "iveskite kiek mokiniu bus vedami duomenys (n>0)" << endl;
 			cin >> n;
-			if (n <= 0) cout << "Pateiktas negalimas skaicius, prasome ivesti vel" << endl;
-		} while (n <= 0);
-		char ans;
+			if(!n) cout << "ivesta netinkama reiksme, prasome ivesti vel" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}while(!n);
+		string ans;
 		do {
 			cout << "Ka norite skaiciuoti ? (v/m), vidurki ar mediana? " << endl;
 			cin >> ans;
-			if (ans != 'v' and ans != 'm') cout << "ivesta netinkama reiksme, prasome ivesti vel " << endl;
-		} while (ans != 'v' and ans != 'm');
+			if (ans != "v" and ans != "m") cout << "ivesta netinkama reiksme, prasome ivesti vel " << endl;
+		} while (ans != "v" and ans != "m");
 		dat* a = new dat[n];
 		for (int i = 0; i < n; i++) input(i+1, a[i], ans);
-		rasymas(a, 10, ans);
+		rasymas(a, kiekis, ans);
 		delete[] a;
 	}
 	
@@ -89,7 +92,7 @@ void rikiavimas(int pazymiai[], int n)
 
 
 
-void input(int i, dat& temp, char answer)
+void input(int i, dat& temp, string answer)
 {
 	do {
 		cout << "iveskite " << i << " -ojo mokinio varda(ilgis ne daugiau 15 simboliu): " << endl;
@@ -102,34 +105,45 @@ void input(int i, dat& temp, char answer)
 		cin >> temp.pavarde;
 		if (temp.pavarde.length() >= 16) cout << "ivesta per ilga pavarde, prasome ivesti trumpesne" << endl;
 	} while (temp.pavarde.length() >= 16);
-	for (int j = 0; j < 10; j++) {
-		cout << "iveskite " << j+1 << " pazimy:" << endl;
-		cin >> temp.pazymiai[j];
+	bool inFail;
+	for (int j = 0; j < kiekis; j++) {
+		do {
+			cout << "iveskite " << j + 1 << " pazimi" << endl;
+			cin >> temp.pazymiai[j];
+			inFail = cin.fail();
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			if (!inFail) if (temp.pazymiai[j] > 10 or temp.pazymiai[j] < 0) inFail = true;
+		} while (inFail);
 	}
 	do {
-		cout << "iveskite egzamino rezultata(0<=egz<=10): " << endl;
+		cout << "iveskite egzamino rezultata" << endl;
 		cin >> temp.egzaminas;
-		if(temp.egzaminas > 10 or temp.egzaminas < 0) cout << "ivesta netinkama reiksme, prasome ivesti dar kart" << endl;
-	} while (temp.egzaminas > 10 or temp.egzaminas < 0);
+		inFail = cin.fail();
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		if (!inFail) if (temp.egzaminas > 10 or temp.egzaminas < 0) inFail = true;
+	} while (inFail);
 	
-	if (answer == 'v')
+	if (answer == "v")
 	{
 		int sum = 0;
-		for (int j = 0; j < 10; j++)
+		for (int j = 0; j < kiekis; j++)
 		{
 			sum += temp.pazymiai[j];
 		}
-		temp.answer = 0.4 * (sum*1.0/10) + 0.6 * temp.egzaminas;
+		temp.answer = 0.4 * (sum*1.0/kiekis) + 0.6 * (temp.egzaminas * 1.0);
 	}
-	else if (answer == 'm')
+	else if (answer == "m")
 	{
-		rikiavimas(temp.pazymiai, 10);
-		temp.answer = temp.pazymiai[5];
+		rikiavimas(temp.pazymiai, kiekis);
+		if (kiekis % 2 == 0) temp.answer = (temp.pazymiai[kiekis / 2] + temp.pazymiai[kiekis / 2 - 1]) * 1.0 / 2 * 0.4 + temp.egzaminas * 1.0 * 0.6;
+		else temp.answer = temp.pazymiai[kiekis / 2]*1.0*0.4+temp.egzaminas*1.0*0.6;
 	}
 
 }
 
-void generavimas(dat& temp, char answer)
+void generavimas(dat& temp, string answer)
 {
 	int index = (double)rand() / RAND_MAX * 16;
 	temp.vardas = vardai[index];
@@ -137,7 +151,7 @@ void generavimas(dat& temp, char answer)
 	temp.pavarde = pavardes[index2];
 	int quantity;
 	do {
-		quantity = (double)rand() / RAND_MAX * 10;
+		quantity = (double)rand() / RAND_MAX * kiekis;
 	} while (quantity == 0);
 	temp.quantity = quantity;
 	int sum = 0;
@@ -149,32 +163,21 @@ void generavimas(dat& temp, char answer)
 	}
 	int egz = (double)rand() / RAND_MAX * 10;
 	temp.egzaminas = egz;
-	if (answer == 'v') temp.answer = sum * 1.0 / quantity*0.4+temp.egzaminas*1.0*0.4;
+	if (answer == "v") temp.answer = sum * 1.0 / quantity*0.4+temp.egzaminas*1.0*0.6;
 	else
 	{
-		int balai[11];
-		for (int i = 0; i <= quantity; i++)
-		{
-			if (i == quantity) balai[i] = temp.egzaminas;
-			else balai[i] = temp.pazymiai[i];
-		}
-		rikiavimas(balai , quantity+1);
-		if (quantity % 2 == 1) temp.answer = balai[(quantity + 1) / 2];
-		else temp.answer = (balai[quantity / 2] + balai[quantity / 2 - 1]) * 1.0/2;
+		rikiavimas(temp.pazymiai , quantity);
+		if (quantity % 2 == 1) temp.answer = temp.pazymiai[quantity / 2];
+		else temp.answer = (temp.pazymiai[quantity / 2] + temp.pazymiai[quantity / 2 - 1]) * 1.0/2*0.4+temp.egzaminas*1.0*0.6;
 	}
 	
 }
 
-void isvedimas(dat temp)
-{
-	cout << left << setw(16) << temp.vardas << left << setw(16) << temp.pavarde << left << setw(7) << fixed<< setprecision(2)<< temp.answer << endl;
-}
-
-void rasymas(dat temp[], int n, char answer)
+void rasymas(dat temp[], int n, string answer)
 {
 	cout << left << setw(16) << "Vardas" << left << setw(16) << "Pavarde"<< "Galutinis";
-	if (answer == 'v') cout << " (Vid.)" << endl;
+	if (answer == "v") cout << " (Vid.)" << endl;
 	else cout << " (Med.)" << endl;
 	cout << "--------------------------------------------------" << endl;
-	for (int i = 0; i < n; i++) isvedimas(temp[i]);
+	for (int i = 0; i < n; i++) cout << left << setw(16) << temp[i].vardas << left << setw(16) << temp[i].pavarde << left << setw(7) << fixed << setprecision(2) << temp[i].answer << endl;
 }
